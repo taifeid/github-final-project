46d08b0dc025c6b86ec52084e1650ed32d2308c8
Taif Bin Eid <80907133+taifeid@users.noreply.github.com>
2023-05-02 20:06:58 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,23 @@
+from machinetranslation import translator
+from flask import Flask, render_template, request
+
+app = Flask("Web Translator")
+
+@app.route("/englishToFrench")
+def englishToFrench():
+    textToTranslate = request.args.get('textToTranslate')
+    translation = translator.english_to_french(textToTranslate)
+    return translation
+
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+    translation = translator.french_to_english(textToTranslate)
+    return translation
+
+@app.route("/")
+def renderIndexPage():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
